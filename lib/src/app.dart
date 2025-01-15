7f975c06ed72197711e9c338fe113dbf8e81d488
soul071213 <js071213asd@gmail.com>
2025-01-15 21:49:59 +0900

    BottomNavigationBar 구현

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:sellshop/src/screens/auth/register.dart';
-import 'package:sellshop/src/screens/feed/show.dart';
+// import 'package:sellshop/src/screens/feed/show.dart';
+import 'package:sellshop/src/screens/home.dart';
 import 'package:sellshop/src/screens/intro.dart';
-import 'package:sellshop/src/screens/shared/data.dart';
-import 'package:sellshop/src/screens/unknown.dart';
+// import 'package:sellshop/src/screens/shared/data.dart';
+// import 'package:sellshop/src/screens/unknown.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -15,22 +16,24 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Carrot Market',
       routes: {
-        '/' : (context) => const Intro(),
+        '/' : (context) => const Home(),
+        '/intro' : (context) => const Intro(),
         '/register' : (context)=> const  Register()
       },
       initialRoute: '/',
-      onGenerateRoute: (route){
-        if(route.name!.startsWith('/feed/')){
-          //경로에서 id를 추출하여 변수에 저장
-          final id = int.parse(route.name!.split('/').last);
-          //추출한 id에 해당하는 항목을 찾습니다.
-          final item=FeedList.firstWhere((e)=>e['id']==id);
 
-          return MaterialPageRoute(builder: (context)=>FeedShow(item));
-        }
+      // onGenerateRoute: (route){
+      //   if(route.name!.startsWith('/feed/')){
+      //     //경로에서 id를 추출하여 변수에 저장
+      //     final id = int.parse(route.name!.split('/').last);
+      //     //추출한 id에 해당하는 항목을 찾습니다.
+      //     final item=FeedList.firstWhere((e)=>e['id']==id);
 
-        return MaterialPageRoute(builder: (context)=>const UnknownScreen());
-      },
+      //     return MaterialPageRoute(builder: (context)=>FeedShow(item));
+      //   }
+
+      //   return MaterialPageRoute(builder: (context)=>const UnknownScreen());
+      // },
     );
   }
 }
